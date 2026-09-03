6d99d3667fdfd11880dd89a211ec1068935d8943
Vanshika3124 <vanshika310.ghaziabad@gmail.com>
2026-09-03 23:09:05 +0530

    Fix Gmail SMTP connection for deployment

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,11 +1,16 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD
-    }
+    },
+
+    family: 4
 });
 
 export const sendEmail = async (to, subject, text, html) => {
