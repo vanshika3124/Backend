4b2e6de62988510d1594b99ae14fa6d6ddb3d1bd
Vanshika3124 <vanshika310.ghaziabad@gmail.com>
2026-09-05 01:34:56 +0530

    added netlify link

--- src/app.js
+++ src/app.js
@@ -13,7 +13,10 @@ const app = express();
 // ==================== CORS ====================
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://bis-sahayaak.netlify.app",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
